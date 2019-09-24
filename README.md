df0c5cb9af52f040fec26f7991e8cfc3d0b017ee
anonymous <anonymous@localhost.localdomain>
2019-09-24 23:45:50 +0530

    exercise 2 solution

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # Node Monodb Excercise
 
+*Exercise 1:*
 - Get all published backend courses
 - Sort them by their name
 - Pick only their name and author
 - Display them
+
+*Exercise 2:*
+  - Get all the published frontend and backend courses
+  - Sort them by their price in a descending order
+  - Pick only their name, author and price
+  - Display them
 
 Data is given in `excercise` directory
   - excercise.json is all data
--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const courseSchema = mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
+/* Exercise 1
 async function getCourses(){
   return await Course.find({isPublished: true, tags: 'backend'})
     .sort({name: 1})
@@ -28,4 +29,20 @@ async function displayCourse(){
   console.log(course);
 }
 
-displayCourse();
+displayCourse(); 
+*/
+
+/* Exercise 2 */
+async function getAuthorAndCourse() {
+  return await Course.find({isPublished: true})
+    .or([{tags: 'frontend'},{tags: 'backend'}])
+    .sort({price: -1})
+    .select({name: 1, author: 1, price: 1});
+}
+
+async function run(){
+  const course = await getAuthorAndCourse();
+  console.log(course);
+}
+
+run();
